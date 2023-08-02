8e19093129b063dbada4b135074394b0f9eeefb8
Diom Shrestha <diomhresth2000@gmail.com>
2023-08-02 16:59:42 +0545

    new

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ let person2 = "Ram";
 person2 = "shyam";
 
 console.log(person2);
+
+//chapter 2
+
+//alert("Username and password is wrong");
+const userName = prompt("enter your user name ");
+const password = prompt("enter your password");
+
+if ((userName === "Diom") & (password === "1234")) {
+  alert("welcome ton our website");
+}
