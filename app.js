83c8e9ca1394747b7d1cee62d119911c2fffbf4b
Diom Shrestha <diomhresth2000@gmail.com>
2023-08-07 18:08:41 +0545

    create module in node

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const http = require("http"); /// declare library
 
+const date = require("./myModule.js");
+
 const myFunction = (req, res) => {
-  res.end("hello world");
+  const dateTime = date();
+  res.end(`hello world ${dateTime}`);
 };
 
-http.createServer(myFunction).listen(4000);
+http.createServer(myFunction).listen(3000);
+console.log("app is running on prot 3000");
 
 /// events, fs, http, https , net , os , fs
