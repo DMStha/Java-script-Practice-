29eceb27ba4067968e82e87592ed1321988a6f5d
Diom Shrestha <diomhresth2000@gmail.com>
2023-08-10 17:40:51 +0545

    Generate QR Code for  the url on the terminal using package qrcode.

--- Node-js/events.js
+++ Node-js/events.js
@@ -1,11 +1,17 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-const file = fs.createReadStream("./data.txt");
+// const file = fs.createReadStream("./data.txt");
 
-file.on("open", (data) => {
-  console.log("data.txt file is open");
-});
+// file.on("open", (data) => {
+//   console.log("data.txt file is open");
+// });
 
-const data = "hello world";
+// const data = "hello world";
 
-fs.writeFileSync("./data.txt", data);
+// fs.writeFileSync("./data.txt", data);
+
+// use moment package to convert current date to 8th aug 2023 format
+// create function that convert "mr raktim shrestha " to "MR RAKTIM SHRESTHA ".
+// use "lodash" package to trim the ehite space form "     mr Raktim Shrestha "
+
+//use upper-case package to convert user inout to upper case using node
