8d3c5bc1652b22038143a22750e95ca7efff0955
Diom Shrestha <diomhresth2000@gmail.com>
2024-01-29 13:11:06 +0545

    0

--- PracticeExercises/index.js
+++ PracticeExercises/index.js
@@ -1,3 +1,10 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
+
+//network port allocate
+
+const port = 6000;
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
